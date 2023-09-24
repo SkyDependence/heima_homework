9250e38dba1458354929b55ac001cc11d0f9ab33
SkyDependence <nanyangzesi@gmail.com>
2023-09-24 20:45:11 +0800

    题目1

---
+++ array.java
@@ -0,0 +1,13 @@
+package heima_homework;
+import java.io.*;
+import java.util.Random;
+import java.util.Scanner;
+
+public class array {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        for(int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
